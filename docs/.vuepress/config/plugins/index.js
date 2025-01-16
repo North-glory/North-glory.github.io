639ec20400a635c8449ac3a67066baa9f7598d53
North-glory <1985448925@qq.com>
2025-01-16 23:54:30 +0800

    记录本体项目代码修改部分

--- docs/.vuepress/config/plugins/index.js
+++ docs/.vuepress/config/plugins/index.js
@@ -117,7 +117,8 @@ function renderCardList(tokens, idx, type) {
         }
 
         if (yamlStr) { // 正确解析出yaml字符串后
-            const dataObj = yaml.safeLoad(yamlStr) // 将yaml字符串解析成js对象
+            // const dataObj = yaml.safeLoad(yamlStr) // 将yaml字符串解析成js对象
+            const dataObj = yaml.load(yamlStr);
             let dataList = []
 
             if (dataObj) { // 正确解析出数据对象
